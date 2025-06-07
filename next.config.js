10cc85f96c7081c61791c1656ee561f65be7a4a6
pryechwad <pryechwad@gmail.com>
2025-06-08 01:11:17 +0530

    fix: remove deprecated appDir config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     domains: ['phosphoricons.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
